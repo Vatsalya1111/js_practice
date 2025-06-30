268130b0f7e9a1ee3909266fbd4b7b2de49164ba
Vatsalya1111 <vatsalya0609@gmail.com>
2025-06-30 04:33:17 +0000

    what is variable and constant

--- basic/variable.js
+++ basic/variable.js
@@ -1,3 +1,9 @@
 console.log("vatsalya")
 console.log('b')
-const 
+const nam="praharsh"
+var emailId="praharsh01@gmail.com"
+let emailPassword="1234"
+accountCity="Bengaluru"
+accountState="karnataka"
+
+console.table([nam,emailId,emailPassword,accountCity,accountState])
