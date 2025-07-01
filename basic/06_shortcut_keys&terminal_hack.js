130bd9ca4b5c14f2c3ceb93d9878a372393248c2
Vatsalya1111 <vatsalya0609@gmail.com>
2025-07-01 10:30:29 +0000

    Shortcut keys and hacks

---
+++ basic/06_shortcut_keys&terminal_hack.js
@@ -0,0 +1,11 @@
+// to run js code in terminal, use node foldername/filename.js
+
+
+// ctrl+shift+p to open the command palette
+
+// ctrl+z to undo 
+// ctrl+y to redo
+
+// ctrl+s to save the current file
+
+// ctrl+` to open the terminal
