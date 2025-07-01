55f93491bae14bf62cafb67432d52c7bc764d648
Vatsalya1111 <vatsalya0609@gmail.com>
2025-07-01 10:29:14 +0000

    operations and comparisons

---
+++ basic/03_operations.js
@@ -0,0 +1,34 @@
+//console.log(2+2)
+//console.log(2**3)
+//console.log(2%3)
+ // 2 is divided by 3 so remaninder is 2.
+
+ str1="hello"
+ str2=" vatsalya"
+ console.log(str1+str2)
+
+ console.log("1"+2)// when one of two is string then it will convert the second one to string and do string concatenation
+ console.log(1+"2")
+
+
+ console.log("1"+2+2) // when string "1"come first then it consider the second no also a string and bz second is string is string third is also consider as string
+
+
+ console.log(1+2+"2") //when number 1 come first then it consider second 2 as number and therefore adds both and then last one is already mention string so do like string concentationation
+
+ console.log(+true) // true is converted to 1
+ console.log(+false) // false is converted to 0
+ //console.log(true+) //give error
+// console.log(false+) //give error 
+
+gamecounter=100
+gamecounter++ //post increment
+console.log(gamecounter) //101
+gamecounter-- //post decrement
+console.log(gamecounter) //100
+
+++gamecounter
+console.log(gamecounter) //101
+--gamecounter
+console.log(gamecounter) //100
+
